81d217fa2e4b34acb2c8ea3763e8b088f38f8f88
luqman <luqmanrijaludien061198@gmail.com>
2019-10-20 19:42:10 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya:\n");
+        array marray = new array();
+        marray.jawaban();
+        System.out.println("\nJawaban Nomer 2:\n");
+        System.out.println("Method : Hal-hal yang bisa di lakukan objeck dari suatu Class. :)");
+        System.out.println("Function : Method yang kita deklarasikan langsung tipe data nya di ikuti variabel dan di ahiri dengan kata Retrun untuk mengambil hasil dari isi method tersebut. :0");
+        System.out.println("Procedur : Method yang di tandai dengan kata void untuk pendefinisian dari suatu method tersebut. :D");
+
+
+
+    }
+
+
+}
---
+++ src/com/company/array.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class array {
+
+
+
+    void jawaban() {
+        String[] nama = {"Luqman","Rijall","Udien"};
+        System.out.println("Luqman RijallUdien : ");
+
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+
+    }
+}
